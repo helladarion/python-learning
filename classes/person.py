8f315b893c2692e3367a28a2f5025013dc57d095
Rafael de Paiva Rogerio <helladarion@gmail.com>
2018-10-24 18:01:39 +0000

    Working status bar and fight with enemy phase. fixed colors on solarized theme, and bars aligned

--- classes/person.py
+++ classes/person.py
@@ -1,7 +1,9 @@
 import random
+from classes.game import bcolor
 
 class Person:
-    def __init__(self, hp, mp, atk, df, magic):
+    def __init__(self, name, hp, mp, atk, df, magic):
+       self.name = name
        self.max_hp = hp
        self.hp = hp
        self.max_mp = mp
@@ -46,9 +48,9 @@ class Person:
 
     def choose_action(self):
         i = 1
-        print("ACTIONS")
+        print("{}ACTIONS{}".format(bcolor.WARNING, bcolor.ENDC))
         for item in self.actions:
-            print(str(i) + ":", item)
+            print("    {}: {}".format(str(i), item))
             i += 1
 
     def choose_magic(self):
@@ -58,8 +60,30 @@ class Person:
             print(str(i) + ":", spell["name"], "(cost:", str(spell["cost"]) +")")
             i += 1
 
-    def show_status(self):
-        print(" HP                       MP                    ")
-        print(" ______________________   ______________________")
-        print("|████████████          | |████████████          |") 
+    def show_status(self, bar_size):
+        name_spaces = " " * (bar_size + 29)
+        # hp bar configuration
+        hp_bar_total = bar_size
+        hp_bar_left = round(self.hp / self.max_hp * hp_bar_total)
+        hp_bar_spaces = " " * (hp_bar_total - hp_bar_left)
+        hp_bar = "█" *  round(self.hp / self.max_hp * hp_bar_total)
+        hp_top = " " * (hp_bar_total + 9)
+        hp_current = hp_bar + hp_bar_spaces
+        hp_value_offset = len(str(self.get_maxhp()))
+        hp_title_spaces = " " * ((len(str(self.get_maxhp()).zfill(hp_value_offset))*2)+5)
+
+        # mp bar configuration
+        mp_bar_total = 20
+        mp_bar_left = round(self.mp / self.max_mp * mp_bar_total)
+        mp_bar_spaces = " " * (mp_bar_total - mp_bar_left)
+        mp_bar = "█" *  round(self.mp / self.max_mp * mp_bar_total)
+        mp_top = " " * (mp_bar_total - 4)
+        mp_current = mp_bar + mp_bar_spaces
+
+        print("{}'s status:{}".format(self.name, name_spaces), end='')
+        yield
+        print("{}{c.BOLD}HP{}MP{}{c.ENDC} ".format(hp_title_spaces, hp_top, mp_top, c=bcolor), end='')
+        yield
+        print("{c.BOLD}{:04}/{:04}{c.ENDC} |{c.OKGREEN}{}{c.ENDC}| {c.BOLD}{:03}/{:03}{c.ENDC} |{c.OKBLUE}{}{c.ENDC}|".format(self.get_hp(), self.get_maxhp(), hp_current, self.get_mp(), self.get_maxmp(), mp_current, c=bcolor), end='')
+        yield
 
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from classes.game import bcolor
 magic = [{"name": "Fire", "cost": 10, "dmg": 100},
          {"name": "Thunder", "cost": 10, "dmg": 100},
          {"name": "Blizzard", "cost": 10, "dmg": 100}]
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200,65, 45, 25, magic)
+player = Person("Jack", 460, 65, 60, 34, magic)
+enemy = Person("Magus", 1100, 125, 45, 25, magic)
 
 running = True
 
@@ -18,17 +18,32 @@ print(bcolor.FAIL + bcolor.BOLD + "AN ENEMY ATTACKS!" + bcolor.ENDC)
 
 while running:
     os.system('clear')
-    player.show_status()
+    player_status, enemy_status = player.show_status(25), enemy.show_status(40)
+    while True:
+        try:
+            next(player_status)
+            print(' ', end='')
+            next(enemy_status)
+            print()
+        except StopIteration:
+            break
     print("============================")
     player.choose_action()
-    choice = input("Chose action:")
+    choice = input("Choose action:")
     index = int(choice) -1
     if index == 0:
         dmg = player.generate_damage()
         enemy.take_damage(dmg)
-        print("You attacked for {} points of damage. Enemy HP: {}".format(dmg, enemy.get_hp()))
-        for i in range(21):
-            sys.stdout.write('\r')
-            sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
-            sys.stdout.flush()
-            sleep(0.25)
+        print("You attacked {} for {} points of damage.".format(enemy.name, dmg))
+        sleep(0.5)
+
+    print("{c.FAIL}{c.BOLD}ENEMY PHASE:{c.ENDC}".format(c=bcolor))
+    dmg = enemy.generate_damage()
+    player.take_damage(dmg)
+    print("{} attacked you for {} points of damage.".format(enemy.name, dmg))
+    sleep(1.5)
+    """ for i in range(21):
+        sys.stdout.write('\r')
+        sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
+        sys.stdout.flush()
+        sleep(0.25)"""
