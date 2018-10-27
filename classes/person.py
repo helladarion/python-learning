d36276e93ce72bceeb7999ff2c0fc0984c1e8707
Rafael de Paiva Rogerio <helladarion@gmail.com>
2018-10-27 22:30:13 +0000

    Generated attack, included running loop, and take_damage

--- classes/person.py
+++ classes/person.py
@@ -1,14 +1,21 @@
+import random
+
 class Person:
-    def __init__(self, hp, mp, magic):
+    def __init__(self, hp, mp, atk, df, magic):
         self.hp = hp
         self.mp = mp
         self.maxhp = hp
         self.maxmp = mp
+        self.atk = atk
+        self.df = df
         self.magic = magic
         self.actions = ["Attack","Magic"]
 
     def get_hp(self):
         return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
 
     def get_mp(self):
         return self.mp
@@ -19,3 +26,14 @@ class Person:
         for act in self.actions:
             print(str(i) + ". " + act)
             i += 1
+
+    def generate_damage(self):
+        atk_high = self.atk + 10
+        atk_low = self.atk - 10
+        return random.randrange(atk_low, atk_high)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+
--- main.py
+++ main.py
@@ -4,7 +4,22 @@ dark_magic = [{"name": "Fire", "cost": 10, "damage": 100},
               {"name": "Thunder", "cost": 10, "damage": 100},
               {"name": "Blizzard", "cost": 10, "damage": 100}]
 
-hero = Person(420, 65, dark_magic)
+player = Person(420, 65, 50, 35, dark_magic)
+enemy = Person(250, 110, 65, 40, [])
 
-hero.show_options()
+#hero.show_options()
 
+running = True
+
+print("Um inimigo se aproximaaaaa")
+while running:
+    player.show_options()
+    choice = input("choose your action: ")
+    index = int(choice)
+    print("You choose {}".format(index))
+    if index == 1:
+        print("You attack the enemy ")
+        atk_dmg = player.generate_damage()
+        enemy.take_damage(atk_dmg)
+        print("You attacked for {} of damage".format(atk_dmg))
+        print("Enemy HP is {}/{}".format(enemy.get_hp(),enemy.get_max_hp()))
