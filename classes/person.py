54c5eff2232b47b68a4682ecebb509c738026acb
Rafael de Paiva Rogerio <helladarion@gmail.com>
2018-11-18 01:13:36 +0000

    better print text style using format, added magic attack, enemy simple attack and status.

--- classes/person.py
+++ classes/person.py
@@ -19,14 +19,38 @@ class Person:
 
     def get_mp(self):
         return self.mp
+    
+    def get_max_mp(self):
+      	return self.maxmp
 
     def show_options(self):
         i = 1
         print("What do you want to do?")
         for act in self.actions:
-            print(str(i) + ". " + act)
+            #print(str(i) + ". " + act)
+            print("{}. {}".format(i, act))
             i += 1
-
+    
+    def show_magic(self):
+        i = 1
+        print("What magic you want to use?")
+        for magic in self.magic:
+            #print(str(i) + ". " + magic["name"])
+            print("{}. {} - cost {}".format(i, magic["name"], magic["cost"]))
+            i += 1
+            
+    def generate_magic_effect(self, magic_id):
+        print("You choose the magic {} this is a magic type {} and with the effect {}".format(self.magic[magic_id]["name"],self.magic[magic_id]["type"],self.magic[magic_id]["effect"]))
+        self.mp -= self.magic[magic_id]["cost"] 
+        if self.magic[magic_id]["type"] == "dark":
+          return self.magic[magic_id]["effect"]
+        else:
+          self.hp += self.magic[magic_id]["effect"]
+          if self.hp > self.maxhp:
+            self.hp = self.maxhp
+          return 0 
+        
+        
     def generate_damage(self):
         atk_high = self.atk + 10
         atk_low = self.atk - 10
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from classes.person import Person
 
-dark_magic = [{"name": "Fire", "cost": 10, "damage": 100},
-              {"name": "Thunder", "cost": 10, "damage": 100},
-              {"name": "Blizzard", "cost": 10, "damage": 100}]
+"""
+TODO
+1. Subtract the df from attack
+2. Include magic on enemy
+3. Add items
+4. Add weapons
+5. Colour
+6. Better status display
 
-player = Person(420, 65, 50, 35, dark_magic)
+""" 
+
+dark_magic = [{"name": "Fire", "cost": 12, "effect": 120, "type": "dark"},
+              {"name": "Thunder", "cost": 10, "effect": 100, "type": "dark"},
+              {"name": "Blizzard", "cost": 15, "effect": 150, "type": "dark"}]
+
+white_magic = [{"name": "Healing", "cost": 5, "effect": 20, "type": "white"},
+               {"name": "Super Heal", "cost": 10, "effect": 50, "type": "white"}]
+
+player_magic = [ dark_magic[0], dark_magic[2], white_magic[1] ]
+
+player = Person(420, 65, 50, 35, player_magic)
 enemy = Person(250, 110, 65, 40, [])
 
 #hero.show_options()
@@ -13,15 +29,27 @@ running = True
 
 print("Um inimigo se aproximaaaaa")
 while running:
+    print("Your HP is {}/{} and your MP is {}/{}".format(player.get_hp(), player.get_max_hp(), player.get_mp(), player.get_max_mp()))
+    print("Enemy HP is {}/{}".format(enemy.get_hp(),enemy.get_max_hp()))
     player.show_options()
     choice = input("choose your action: ")
     index = int(choice)
-    print("You choose {}".format(index))
+    #print("You choose {}".format(index))
     if index == 1:
         print("You attack the enemy ")
         atk_dmg = player.generate_damage()
         enemy.take_damage(atk_dmg)
         print("You attacked for {} of damage".format(atk_dmg))
-        print("Enemy HP is {}/{}".format(enemy.get_hp(),enemy.get_max_hp()))
-        
+    elif index == 2:
+        player.show_magic()
+        choice_spell = input("Choose your spell")
+        spell = int(choice_spell) - 1
+        spell_effect = player.generate_magic_effect(spell)
+        if spell_effect > 0:
+            enemy.take_damage(spell_effect)
+            print("You attacked using {} for {} of damage".format(player.magic[spell]["name"], player.magic[spell]["effect"]))
+    # Enemy Turn
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("The enemy attacked you and you took {} of damage".format(enemy_dmg))
         
